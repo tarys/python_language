f32ab3cb56036b87d88095348359585473b0e564
Taras Slipets <taras.slipets@gmail.com>
2016-09-27 23:33:29 +0300

    proper formatting applied to homeworks_skeleton.py

--- homeworks_skeleton.py
+++ homeworks_skeleton.py
@@ -4,15 +4,15 @@ import os
 with open('students.yml', 'r') as students_list_file:
     students = yaml.load(students_list_file)
 
-for group in students['groups']:
-    for student in students['groups'][group]:
-        student_directory = './students/' + group + '/' + student
+    for group in students['groups']:
+        for student in students['groups'][group]:
+            student_directory = './students/' + group + '/' + student
 
-        if not os.path.isdir(student_directory):
-            os.makedirs(student_directory)
+            if not os.path.isdir(student_directory):
+                os.makedirs(student_directory)
 
-        file_name = student_directory + '/welcome.py'
-        if not os.path.isfile(file_name):
-            file = open(file_name, "w")
-            file.write('print(\'Welcome aboard, {}!\')\n'.format(student.split('_')[1]))
-            file.close()
+            file_name = student_directory + '/welcome.py'
+            if not os.path.isfile(file_name):
+                file = open(file_name, "w")
+                file.write('print(\'Welcome aboard, {}!\')\n'.format(student.split('_')[1]))
+                file.close()
