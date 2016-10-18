7241bf7cf3ce7db1266b16bd1328ab3ef077a0a2
DeadSpoon <mrdeadspoon@ya.ru>
2016-10-18 19:53:36 +0300

    1. Введення та вивід інформації. Прості операції
    Тема:"Введення та вивід інформації. Прості операції "
    Варіант:загальний

---
+++ students/km61/Artem_Morozov/homework_1.py
@@ -0,0 +1,103 @@
+#task1------------------------------------------------------------
+"""
+Напишите программу, которая считывает три числа и выводит их сумму. Каждое число записано в отдельной строке.
+"""
+#Program calculate sum of three numbers
+sum=0
+number=int(input('Enter first number'))                                                                                                                       
+sum+=number
+number=int(input('Enter second number'))
+sum+=number
+number=int(input('Enter third number'))
+sum+=number
+print('Sum of three numbers=',sum)
+#-----------------------------------------------------------------
+
+
+#task2------------------------------------------------------------
+"""
+Напишите программу, которая считывает длины двух катетов в прямоугольном треугольнике и выводит его площадь. Каждое число записано в отдельной строке.
+"""
+#Program calculates area of a right triangle
+first_cathetus=float(input('Enter first cathetus'))
+second_cathetus=float(input('Enter second cathetus'))
+print('Area of triangle=',second_cathetus*first_cathetus/2)
+#-----------------------------------------------------------------
+
+
+#task3------------------------------------------------------------
+"""
+n школьников делят k яблок поровну, неделящийся остаток остается в корзинке. 
+Сколько яблок достанется каждому школьнику? 
+Сколько яблок останется в корзинке?
+Программа получает на вход числа n и k и должна вывести искомое количество яблок (два числа).
+"""
+#Program prints amount of apples pupils will get, and amount of apples what will be in a basket
+pupils_amount=int(input('Enter pupils amount'))
+apples_amount=int(input('Enter apples amount'))
+print('Amount of apples pupils will get=',apples_amount//pupils_amount)
+print('Amount of apples what will be in a basket=',apples_amount%pupils_amount)
+
+#-----------------------------------------------------------------
+
+
+#task4------------------------------------------------------------
+"""
+Дано число n. 
+С начала суток прошло n минут. 
+Определите, сколько часов и минут будут показывать электронные часы в этот момент. 
+Программа должна вывести два числа: количество часов (от 0 до 23) и количество минут (от 0 до 59). 
+Учтите, что число n может быть больше, чем количество минут в сутках.
+"""
+#Program prints time on clocks after n minutes passed 
+minutes_amount=int(input('Enter amount of passed minutes'))
+minutes_amount=minutes_amount%1440
+print('Time on clocks',minutes_amount//60,minutes_amount%60)
+
+#-----------------------------------------------------------------
+
+
+
+#task5------------------------------------------------------------
+"""
+Напишите программу, которая приветствует пользователя, выводя слово Hello, введенное имя и знаки препинания по образцу:
+"""
+#Program says hello to user 
+user_name=input('Enter your name')
+print("Hello,",user_name+"!")
+
+#-----------------------------------------------------------------
+
+
+
+#task6------------------------------------------------------------
+"""
+Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).
+"""
+#Program prints previous and next numbers for entered number
+number=int(input('Enter number'))
+print("The next number for the number",number,"is",str(number+1)+".")
+print("The previous number for the number",number,"is",str(number-1)+".") 
+
+#-----------------------------------------------------------------
+
+
+
+#task7------------------------------------------------------------
+"""
+В школе решили набрать три новых математических класса. 
+Так как занятия по математике у них проходят в одно и то же время, было решено выделить кабинет для каждого класса и купить в них новые парты. 
+За каждой партой может сидеть не больше двух учеников. 
+Известно количество учащихся в каждом из трёх классов. 
+Сколько всего нужно закупить парт чтобы их хватило на всех учеников?
+Программа получает на вход три натуральных числа: количество учащихся в каждом из трех классов.
+"""
+#Program prints amount of desks needed for entered pupils amount
+pupils_amount=int(input('Enter pupils amount in first form'))
+desk_amount=pupils_amount//2+pupils_amount%2
+pupils_amount=int(input('Enter pupils amount in second form'))
+desk_amount=desk_amount+pupils_amount//2+pupils_amount%2
+pupils_amount=int(input('Enter pupils amount in third form'))
+desk_amount=desk_amount+pupils_amount//2+pupils_amount%2
+print('Needed desk amount='desk_amount)
+#-----------------------------------------------------------------
