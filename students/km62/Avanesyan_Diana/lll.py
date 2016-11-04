cbbbd299c70d550ddd3b90e0923526b3afa0e527
Diana <dianaubermensch@gmail.com>
2016-11-04 03:45:56 +0200

    Update lll.py

--- students/km62/Avanesyan_Diana/lll.py
+++ students/km62/Avanesyan_Diana/lll.py
@@ -1,6 +1,4 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print ('Home work №5') 
+print ('Home work №5') 
 print() 
 
  #task1 
