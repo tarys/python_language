fb330422ffcad0dc8c249f0a601813704377c085
buchynska <adolphina13@gmail.com>
2016-11-09 20:56:09 +0200

    Update homework_5.py

--- students/km63/Buchynska_Kateryna/homework_5.py
+++ students/km63/Buchynska_Kateryna/homework_5.py
@@ -6,12 +6,12 @@
 distance(x1, y1, x2, y2), вычисляющая расстояние между точкой (x1,y1) и (x2,y2).
 Считайте четыре действительных числа и выведите результат работы этой функции. 
 """ 
-x1=float(input('Enter x1')) 
-y1=float(input('Enter y1')) 
-x2=float(input('Enter x2')) 
-y2=float(input('Enter y2')) 
+x1 = float(input('Enter x1')) 
+y1 = float(input('Enter y1')) 
+x2 = float(input('Enter x2')) 
+y2 = float(input('Enter y2')) 
 def distance(x1, y1, x2, y2): 
-  a=(((x2-x1)**2)+((y2-y1)**2))**(1/2) 
+  a = (((x2-x1)**2)+((y2-y1)**2))**(1/2) 
   return a 
 print (distance(x1,y1,x2,y2)) 
 
@@ -33,7 +33,7 @@ def power(a, n):
             return 1 / res 
     a=float(input()) 
     n=int(input()) 
-print(power( a, n))) 
+print (power( a, n))) 
 #---------------------------------------------------------------— 
 
 
@@ -51,10 +51,10 @@ def power(a, n):
             return res 
         else: 
             return 1 / res 
-a=float(input()) 
-n=int(input()) 
-print(power(a,n))
-print(y) 
+a = float(input()) 
+n = int(input()) 
+print (power(a,n))
+print (y) 
 
 #---------------------------------------------------------------— 
 
@@ -70,5 +70,5 @@ def fib(n):
     else: 
         return fib(n - 1) + fib(n - 2) 
 
-print(fib(int(input()))) 
+print (fib(int(input()))) 
 #---------------------------------------------------------------—
