3a6527d802381d8552e10065f1aeaa506576488f
Diana <dianaubermensch@gmail.com>
2016-12-02 07:33:51 +0200

    1. Введення та вивід інформації. Прості операції

---
+++ students/km62/Avanesyan_Diana/Home1.py
@@ -0,0 +1,30 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+a = int(input( ))
+b = int(input( ))
+c = int(input( ))
+print(a+b+c)
+
+a=int(input( ))0
+b= int(input( ))
+print((a*b)/2)
+
+«Яблука»
+N=int(input('вв...студ '))
+K=int(input('вв..ябл '))
+print(divmod(K,N))
+             N=int(input('Введіть кількість хвилин'))
+               print(divmod(N,60))
+
+          name=input(“Enter your name: ”)
+ print(“Hello”, name, «!»)
+
+          N=int(input(“Enter number “))
+       	 Print(“The next number for the number”, N, “is” N+1)
+	 Print(“The previous number for the number N, “is” N-1)
+
+            a=int(input('Введіть кількість учнів у першому класі'))
+  b=int(input('Введіть кількість учнів у другому класі'))
+  c=int(input('Введіть кількість учнів у третьому класі'))
+  print((a+b+c)/2)
